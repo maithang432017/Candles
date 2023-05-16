4709b098e9bfca5c195e8f94c40209f53df095dd
maithang432017 <maithang432017@gmail.com>
2023-05-17 05:48:49 +0700

    candles

---
+++ O5/src/CandleOddSquare/Candles.java
@@ -0,0 +1,29 @@
+package CandleOddSquare;
+
+import java.util.List;
+import java.util.ArrayList;
+public class Candles {
+	public static void main(String[] args) {
+		int n=5;
+		List<Integer> numOfCandles= new ArrayList<>();
+		numOfCandles.add(4);
+		numOfCandles.add(4);
+		numOfCandles.add(2);
+		numOfCandles.add(3);
+		System.out.println("number of highest candles:"+candles(numOfCandles));
+			}
+public static int candles(List<Integer> arr) {
+	int n=arr.size();
+	int highest=arr.get(0);
+	int count=0;
+	for (int i=0;i<n;i++) {
+		if(highest<arr.get(i)) {
+			highest=arr.get(i);
+			count=1;
+		}
+		if(highest==arr.get(i)) {
+			count++;
+		}
+	}return count;
+}
+}
